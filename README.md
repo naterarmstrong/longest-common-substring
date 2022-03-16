127793ee6766ee3a222916c59b8f0b83d6673975
Nate Armstrong <naterarmstrong@gmail.com>
2022-03-15 20:54:18 -0600

    Initial commit playing around with problem
    Use Kasai construction algorithm to get LCS between suffixes of a
    combined string. No logic yet to ensure that the LCS is not just between
    different suffixes of the same string. Gets the suffix array
    construction from a library import.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Rust by Implementing Longest Common Substring
+
+This is a repo to learn rust by implementing an algorithm for finding the longest common substring which uses suffix arrays / trees.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use suffix::SuffixTable;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let s1 = "abcdef".to_string();
+    let s2 = "xyzabcdfe".to_string();
+    let sentinel = '!';
+
+    let combined = format!("{}{}{}", s1, sentinel, s2);
+
+    let st = SuffixTable::new(&combined);
+
+    // maybe implement manber-myers algorithm?
+    let lcp = get_lcp_array(&combined, st.table());
+    let max_and_index = lcp.iter().enumerate().max_by(|(_, x), (_, y)| x.cmp(y)).unwrap();
+    let start_index = st.table()[max_and_index.0] as usize;
+
+    println!("{}", &combined);
+    println!("{:?}", st);
+    println!("{:?}", st.table());
+    println!("{:?}", &lcp);
+    println!("{:?}", max_and_index);
+    println!("{}", &combined.as_str()[start_index..start_index + max_and_index.1]);
+}
+
+
+// Kasai construction algo
+fn get_lcp_array(original_text: &String, suffix_array: &[u32]) -> Vec<usize> {
+    let n = suffix_array.len();
+    let s = original_text.as_bytes();
+    let mut sa_inv: Vec<usize> = vec![0; n];
+
+    for i in 0..n {
+        sa_inv[suffix_array[i] as usize] = i;
+    }
+
+    let mut lcp: Vec<usize> = vec![0; n];
+
+    let mut k = 0;
+
+    for i in 0..n {
+        if sa_inv[i] == n - 1 {
+            k = 0;
+            continue;
+        }
+
+        // Next substring
+        let j: usize = suffix_array[sa_inv[i] + 1] as usize;
+
+        while (i + k < n) && (j + k < n) && s[i + k] == s[j + k] {
+            k += 1;
+        }
+
+        lcp[sa_inv[i]] = k;
+
+        if k > 0 {
+            k -= 1;
+        }
+    }
+
+    return lcp;
 }
